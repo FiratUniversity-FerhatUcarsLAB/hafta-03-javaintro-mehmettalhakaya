bbd8c4846891c9fe4efd1a611b0a59710f5ab5e3
mehmettalhakaya <250542009@firat.edu.tr>
2025-10-20 16:30:23 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,10 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+        System.out.println("Urun      Miktar     Fiyat      Toplam");
+        System.out.println("----------------------------------------");
+        System.out.println("Ekmek     2          10         20");
+        System.out.println("Süt       3          25         75");
+        System.out.println("Dolma     1          25         25");
+        System.out.println("----------------------------------------");
+    }
+}
