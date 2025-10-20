f9fec4c91e5707352e96abb852e5816048e698f1
mehmettalhakaya <250542009@firat.edu.tr>
2025-10-20 11:11:38 +0300

    Update IkiIslem.java

--- Task-2-Matematikselislemler/IkiIslem.java
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -1,6 +1,8 @@
 public class IkiIslem {
     public static void main(String[] args) {
-        int sonuc = 1+2+3+4+5+6+7+8+9;
-        System.out.println(sonuc);
+        double pay = (9.5*4.5) - (2.5*3);
+        double payda = 45.5 - 3.5;
+        double sonuc = pay / payda;
+        System.out.printf("%.4f\n", sonuc);
     }
 }
