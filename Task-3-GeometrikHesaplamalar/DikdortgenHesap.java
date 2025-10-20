27f42171d175eabc87f15b7751f961b2f8c8dc20
mehmettalhakaya <250542009@firat.edu.tr>
2025-10-20 11:53:40 +0300

    Update DikdortgenHesap.java

--- Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -1,7 +1,7 @@
 public class DikdortgenHesap {
     public static void main(String[] args) {
         double En = 4.5;
-        double Boy = 6.9;
+        double Boy = 7.9;
         double Alan = En * Boy;
         double Cevre = En * Boy;
 
