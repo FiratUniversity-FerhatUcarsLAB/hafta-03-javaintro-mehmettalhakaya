cf67346c2d5afb082180d2210997a3c93cd6d18e
mehmettalhakaya <250542009@firat.edu.tr>
2025-10-20 16:42:30 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,14 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+        double katsayi = 1.609; // 1 mil = 1.609 km
+
+        System.out.println("Mil     Kilometre");
+        System.out.println("------------------");
+
+        System.out.println("1       " + (1 * katsayi));
+        System.out.println("5       " + (5 * katsayi));
+        System.out.println("10      " + (10 * katsayi));
+        System.out.println("20      " + (20 * katsayi));
+        System.out.println("50      " + (50 * katsayi));
+    }
+}
