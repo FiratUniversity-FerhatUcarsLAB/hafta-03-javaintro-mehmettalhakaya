fbb3c6791ac16c950327b2da8568bd991e97e396
mehmettalhakaya <250542009@firat.edu.tr>
2025-10-20 08:19:12 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,4 +1,4 @@
-public class Main {
+public class BesKereTekrar {
     public static void main(String[] args) {
         System.out.println("Java'ya Hosgeldiniz");
         System.out.println("Java'ya Hosgeldiniz");
